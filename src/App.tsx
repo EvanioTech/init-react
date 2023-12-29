6ce092d71836960d47c5672a182b0ad337c68a86
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-29 16:01:44 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { Card } from "./components/Card";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
+    
     <div>
+    <Header />
     hello world
     <Card id={1}
     paragraph="typescript"
     details="TS pra front and back" />
     <Card id={2}
     paragraph="HTML"
-    details="HTML para frontend"/>
+    details="HTML para frontend "
+    />
     
     
      </div> 
     
-    <div>
-      footer 
-    </div>
+    <Footer/>
     </>
   );
 }
