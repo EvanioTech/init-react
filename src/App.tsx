de4dc65a681323c7410dc2ff5fb1d241a7dc4172
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-12-29 15:48:15 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import { Card } from "./components/Card";
 
 function App() {
   return (
-    <div> hello world
-    <div><Card /></div>
+    <>
+    <div>
+    hello world
+    <Card id={1}
+    paragraph="typescript"
+    details="TS pra front and back" />
+    <Card id={2}
+    paragraph="HTML"
+    details="HTML para frontend"/>
+    
+    
+     </div> 
+    
+    <div>
+      footer 
     </div>
+    </>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,17 @@
-export const Card = () => {
+interface Icard {
+    id:number ,
+    paragraph:string,
+    details:string
+}
+
+
+export const Card = ({id,paragraph, details}: Icard) => {
+    
     return(
         <div>
+            <h1>Hello world {id}</h1>
+            <p>{paragraph}</p>
+            <p>{details}</p>
            Component Card
         </div>
     )
